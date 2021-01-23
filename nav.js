b79c604c37dd1d3653d5a6d863994c3266abea9b
Ben Muchiri <bernard.njoroge@strathmore.edu>
2021-01-22 17:19:17 -0800

    Side Nav

--- nav.js
+++ nav.js
@@ -1,14 +1,20 @@
 const menuBtn = document.querySelector(".menu-btn");
+const sideNav = document.querySelector(".side-navbar");
 
 let menuOpen = false;
+let navOpen = false;
 
 //responsive navigation
 menuBtn.addEventListener("click", () => {
     if (!menuOpen) {
         menuBtn.classList.add("open");
+        sideNav.classList.add("open");
         menuOpen = true;
+        navOpen = true;
       } else {
         menuBtn.classList.remove("open");
+        sideNav.classList.remove("open");
         menuOpen = false;
+        navOpen = false;
       }
 });
